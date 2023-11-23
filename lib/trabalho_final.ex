f9eec3f4d3ae73c595ab2925e7aab08f3c96c101
luiizgcl <luiizgcl@gmail.com>
2023-11-22 22:04:34 -0300

    chore: Dados por Input

--- lib/trabalho_final.ex
+++ lib/trabalho_final.ex
@@ -2,13 +2,29 @@ defmodule TrabalhoFinal do
   import IntervaloConfianca
   import TamanhoAmostra
 
+  # Proporção
   def intervalo_confianca() do
-    {valor_menor, valor_maior} = calcular_intervalo_confianca(465, 500, 95)
+    IO.puts("Digite o valor da amostra: ")
+    {amostra, _} = Integer.parse(IO.read(:stdio, :line))
+    IO.puts("Digite o valor da população: ")
+    {populacao, _} = Integer.parse(IO.read(:stdio, :line))
+    IO.puts("Digite o nível de confiança em %: ")
+    {nivel_confianca, _} = Integer.parse(IO.read(:stdio, :line))
+
+    {valor_menor, valor_maior} = calcular_intervalo_confianca(amostra, populacao, nivel_confianca)
     IO.puts("IC=(#{valor_menor}; #{valor_maior})")
   end
 
+  #Proporção
   def tamanho_amostra() do
-    n = calcular_tamanho_amostra(100, 1250, 99)
+    IO.puts("Digite o valor da media amostral: ")
+    {media_amostral, _} = Integer.parse(IO.read(:stdio, :line))
+    IO.puts("Digite o valor do Desvio Padrão Populacional: ")
+    {sigma, _} = Integer.parse(IO.read(:stdio, :line))
+    IO.puts("Digite o nível de confiança em %: ")
+    {nivel_confianca, _} = Integer.parse(IO.read(:stdio, :line))
+
+    n = calcular_tamanho_amostra(media_amostral, sigma, nivel_confianca)
     IO.puts("TA=#{n}")
   end
 end
